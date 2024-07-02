40c80ae40546f1a53c588b1de217db8c459bbfa6
Quentin D <quentin.deschamps@webwag.com>
2024-07-02 09:56:12 +0200

    Initial Commit

---
+++ geevtest/geevtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  geevtestApp.swift
+//  geevtest
+//
+//  Created by Quentin Deschamps on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct geevtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
